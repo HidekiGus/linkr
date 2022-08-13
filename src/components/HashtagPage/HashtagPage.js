ce944a2629f39c73f83468445f1bc5b00af64f2a
dr1co <dricomatoba@hotmail.com>
2022-08-13 11:08:17 -0300

    added token requirement

--- src/components/HashtagPage/HashtagPage.js
+++ src/components/HashtagPage/HashtagPage.js
@@ -1,18 +1,22 @@
 import axios from 'axios';
 import styled from 'styled-components';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useContext } from 'react';
 import { useParams } from 'react-router-dom';
 
 import Header from '../Header/Header.js';
 import reqRoot from '../../utils/reqRoot';
+import UserContext from '../../contexts/UserContext.js';
+import generateHeader from '../../utils/TokenHeaders.js';
 
 export default function HashtagPage() {
     const [posts, setPosts] = useState([]);
     const { hashtag } = useParams()
+    const { user } = useContext(UserContext);
+    const config = generateHeader(user);
 
     useEffect(() => {
         try {
-            const promise = await axios.get(`${reqRoot}/hashtag/${hashtag}`);
+            const promise = await axios.get(`${reqRoot}/hashtag/${hashtag}`, config);
             setPosts(promise.data);
         } catch (err) {
             console.log(err.response);
