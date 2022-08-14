09beaeba5310ccdf4a4f252402d31aec3c1925b5
HidekiGus <40206355+HidekiGus@users.noreply.github.com>
2022-08-13 22:49:25 -0300

    feat: adiciona verificações sobre os posts recebidos do backend

--- src/components/Timeline/timeline.js
+++ src/components/Timeline/timeline.js
@@ -1,17 +1,29 @@
 import styled from "styled-components";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import Div from "./Div.js";
 import Header from "../Header/Header.js";
 import { Link ,useNavigate } from "react-router-dom";
 import jooj from "./jooj.png";
+import { ReactTagify } from "react-tagify";
 
 
 export default function Timeline(){
-    const [pesq,setPesq]=useState('')
-    const [chave,setChave]=useState('teste')
-    const [res,setRes]=useState([])
-    const caixa=['t','e','s','t','e']
+    const [pesq,setPesq]=useState('');
+    const [chave,setChave]=useState('teste');
+    const [res,setRes]=useState([]);
+    const caixa=['t','e','s','t','e'];
+    const [ posts, setPosts ] = useState(null);
+    const [ error, setError ] = useState(null);
+
+    useEffect(() => {
+        const promise = axios.get("http://localhost:4000/posts");
+        promise.then((res) => setPosts(res.data));
+        promise.catch((err) => setError(err));
+    }, []);
+
+    console.log(posts);
+
     async function pesquisa(){
         if(pesq.length <3){
             return
@@ -53,10 +65,26 @@ export default function Timeline(){
                         <TextInputBox />
                     </UserTextBoxes>
                 </UserPublish>
-                    <Post></Post>
-                    <Post></Post>
-                    <Post></Post>
-                    <Post></Post>
+                    {posts === null && error === null ? <h1>Loading</h1> : 
+                        posts === null && error !== null ? <h1>An error occured while trying to fetch the posts, please refresh the page</h1> :
+                            posts.length === 0 ? <h1>There are no posts yet</h1> :
+                                posts.map((post, index) => { return (
+                                    <Post key={index}>
+                                        <PostUserPicture src={post.image}/>
+                                        <PostTextBoxes>
+                                            <PostUserName>{post.name}</PostUserName>
+                                            <PostUserText>{post.description}</PostUserText>
+                                        </PostTextBoxes>
+                                    </Post>
+                                )})
+                    }
+                    <Post>
+                        <PostUserPicture src={jooj}/>
+                        <PostTextBoxes>
+                            <PostUserName>Joãozinho Joãozão</PostUserName>
+                            <PostUserText>XANGARINGA xungaringa ringa ringa xangari xunga xaringa xarangara xangara</PostUserText>
+                        </PostTextBoxes>
+                    </Post>
                 </PostsContainer>
             </Body>
         
@@ -67,14 +95,14 @@ export default function Timeline(){
 const Container = styled.div`
     width: 100vw;
     height: 100vh;
-    background-color:#333333;
     position: relative;
 `;
 
 const Body = styled.div`
     width: 100vw;
-    height: auto;
-    min-height: calc(100vh - 70px);
+    height: fit-content;
+
+    background-color:#333333;
     display: flex;
     align-items: center;
     justify-content: flex-start;
@@ -86,7 +114,6 @@ const PostsContainer = styled.div`
     width: 600px;
     min-height: calc(100vh -70px);
     height: auto;
-    background-color: red;
     padding-top: 100px;
     display: flex;
     align-items: center;
@@ -98,7 +125,6 @@ const PostsContainer = styled.div`
 const TimelineTextContainer = styled.div`
     height: 120px;
     width: 100%;
-    background-color: aqua;
 
     font-size: 40px;
     display: flex;
@@ -120,9 +146,7 @@ const UserPublish = styled.div`
     input {
         width: 80%;
         height: 40px;
-
     }
-
 `
 
 const UserPicture = styled.img`
@@ -165,11 +189,61 @@ const TextInputBox = styled.input`
 `
 
 const Post = styled.div`
-    min-height: 200px;
+    min-height: 300px;
     height: auto;
     width: 100%;
 
-    background-color: blue;
-    margin-bottom: 20px;
+    background-color: white;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
 
+    border-radius: 16px;
+    margin: 10px 0;
+
+    background-color: #171717;
+
+    input {
+        width: 80%;
+        height: 40px;
+    }
+`
+
+const PostUserPicture = styled.img`
+    height: 50px;
+    width: 50px;
+    border-radius: 27px;
+    margin: 10px auto 220px;
+`
+
+const PostUserName = styled.div`
+    height: 20px;
+    width: 95%;
+    color: #FFFFFF;
+    font-size: 20px;
+`
+
+const PostUserText = styled.div`
+    min-height: 30px;
+    height: auto;
+    width: 95%;
+    color: #B7B7B7;
+    font-size: 17px;
+
+    margin: 15px 0;
+`
+
+const PostTextBoxes =  styled.div`
+    height: 100%;
+    width: 85%;
+    display: flex;
+    align-items: center;
+    justify-content: space-evenly;
+    flex-direction: column;
+`
+
+const Snippet = styled.div`
+    width: 95%;
+    height: 150px;
+    background-color: white;
 `
