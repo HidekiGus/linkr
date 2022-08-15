536ab5c33a9bc6b53a6f83dec8c5b99b6c165a36
daniel <daniel.bnh13@gmail.com>
2022-08-15 17:14:40 -0300

    a

--- src/utils/reqRoot.js
+++ src/utils/reqRoot.js
@@ -1,6 +1,6 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-const reqRoot = 'http://localhost:4000';
+const reqRoot = 'https://projetao-linkr-backend.herokuapp.com/';
 
 export default reqRoot;
